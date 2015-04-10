e0ad3188afb39e5aad34328dcdbb4e1b4acb611d
Maarten Everts <me@nn8.nl>
2015-04-10 15:17:05 +0200

    Add initial simple example code.

---
+++ examples/p1read.go
@@ -0,0 +1,57 @@
+// Simple example program that illustrates how to use the dsmr4p1 library.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/mhe/dsmr4p1"
+	"github.com/tarm/serial"
+	"log"
+	"os"
+
+	"io"
+)
+
+var testfile = flag.String("testfile", "", "Testfile to use instead of serial port")
+var device = flag.String("device", "/dev/ttyUSB0", "Serial port device to use")
+var baudrate = flag.Int("baud", 115200, "Baud rate to use")
+
+func main() {
+	fmt.Println("p1read")
+	flag.Parse()
+
+	var input io.Reader
+
+	var err error
+	if *testfile == "" {
+		c := &serial.Config{Name: *device, Baud: *baudrate}
+		input, err = serial.OpenPort(c)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		input, err = os.Open(*testfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	ch := dsmr4p1.Poll(input)
+	for t := range ch {
+		r, err := t.Parse()
+
+		fmt.Println("Received telegram")
+
+		timestamp := r["0-0:1.0.0"][0]
+		ts, err := dsmr4p1.ParseTimestamp(timestamp)
+		if err != nil {
+			fmt.Println("Error in time parsing:", err)
+			continue
+		}
+		fmt.Println("Timestamp:", ts)
+		fmt.Println("Electricty power delivered:", r["1-0:1.7.0"][0])
+		fmt.Println("Electricty power received: ", r["1-0:2.7.0"][0])
+		fmt.Println()
+	}
+	fmt.Println("Done. Exiting.")
+}
