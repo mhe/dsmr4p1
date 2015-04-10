a636ef38f244cee1dbdcf50321d8cb8a43f51470
Maarten Everts <me@nn8.nl>
2015-04-10 14:18:00 +0200

    Initial code commit.

---
+++ dsmr4p1.go
@@ -0,0 +1,121 @@
+package dsmr4p1
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"github.com/howeyc/crc16"
+)
+
+// Compute and return the CRC16 checksum over the byte slice in data.
+// According to the DSMR 4.0.4 spec, this CRC16 uses the polynomial
+// x^16 + x^15 + x^2 + 1, which is the same polynomial as in CRC16-IBM.
+// However, we cannot simply use the checksum of the crc16 package as this version
+// of the spec (as opposed to the 4.0 version) also states: "CRC16 uses no XOR in,
+// no XOR out and is computed with least significant bit first." This code is the same
+// as the "update" function in the crc16 function minus the initial and final XOR operations.
+func p1crc16(data []byte) (crc uint16) {
+	crc = 0
+	for _, v := range data {
+		crc = crc16.IBMTable[byte(crc)^v] ^ (crc >> 8)
+	}
+	return
+}
+
+// Constants for now as I'm assuming all dutch smartmeters will be in the
+// same Dutch timezone.
+const (
+	summerTimezone = "CEST"
+	winterTimezone = "CET"
+)
+
+// Parse the timestamp format used in the dutch smartmeters. Do note this function
+// assumes the CET/CEST timezone.
+func ParseTimestamp(timestamp string) (time.Time, error) {
+	// The format for the timestamp is:
+	// YYMMDDhhmmssX
+	// The value used for X determines whether DST is active.
+	// S (summer?) means yes, W (winter?) means no.
+
+	var timezone string
+	switch timestamp[len(timestamp)-1] {
+	case 'S':
+		timezone = summerTimezone
+	case 'W':
+		timezone = winterTimezone
+	default:
+		return time.Time{}, errors.New("Error parsing timestamp: missing DST indicator")
+	}
+
+	timestamp = timestamp[:len(timestamp)-1] + " " + timezone
+	ts, err := time.Parse("060102150405 MST", timestamp)
+	if err != nil {
+		return ts, err
+	}
+	return ts, nil
+}
+
+// Starts polling and attempts to parse a telegram.
+func startPolling(input io.Reader, ch chan Telegram) {
+	br := bufio.NewReader(input)
+	for {
+		// Read until we find a '/', which should be the beginning of the telegram.
+		_, err := br.ReadBytes(byte('/'))
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		// Unread the byte as the '/' is also part of the CRC computation.
+		err = br.UnreadByte()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		// The '!' character signals the end of the telegram.
+		data, err := br.ReadBytes(byte('!'))
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		// The four hexadecimal characters are the CRC-16 of the preceding data, delimitted by
+		// a carriage return.
+		crcBytes, err := br.ReadBytes(byte('\n'))
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if len(crcBytes) != 6 {
+			log.Println("Unexpected number of CRC bytes.")
+			continue // Maybe we can recover?
+		}
+		dataCRC := string(crcBytes[:4])
+		computedCRC := fmt.Sprintf("%04X", p1crc16(data))
+
+		if dataCRC == computedCRC {
+			t := Telegram(data)
+			ch <- t
+		} else {
+			log.Printf("CRC values do not match: %s vs %s\n", dataCRC, computedCRC)
+		}
+	}
+	// Close the channel (should only happen with EOF, allows for clean exit).
+	close(ch)
+}
+
+// Start polling the P1 port. This function will start a goroutine and received telegrams are
+// put into returned channel. Only telegrams whose CRC value are correct are put into
+// the channel.
+func Poll(input io.Reader) chan Telegram {
+	ch := make(chan Telegram)
+	go startPolling(input, ch)
+	return ch
+}
---
+++ telegram.go
@@ -0,0 +1,56 @@
+package dsmr4p1
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+)
+
+// Type for a P1 telegram. It is essentially a slice of bytes.
+type Telegram []byte
+
+// Return the identifier in the telegram
+func (t Telegram) Identifier() string {
+	// According to the documentation, the telegram starts with:
+	// "/XXXZ Ident CR LF CR LF", followed by the data.
+	i := bytes.Index(t, []byte("\r\n\r\n"))
+	return string(t[5:i])
+}
+
+// Attempt to parse the telegram. Return a map of strings to string slices. The keys
+// in the map are the ID-codes, the strings in the slice are the value between brackets
+// for that ID-code.
+func (t Telegram) Parse() (map[string][]string, error) {
+	// Parse the telegram in a relatively naive way. Of course this
+	// is not properly langsec approved :)
+
+	lines := strings.Split(string(t), "\r\n")
+
+	// Some additional checks
+	if lines[0][0] != '/' {
+		return nil, errors.New("Expected '/' missing in first line of telegram")
+	}
+	if len(lines[1]) != 0 {
+		return nil, errors.New("Missing separating new line (CR+LF) between identifier and data in telegram.")
+	}
+
+	result := make(map[string][]string)
+	// Iterate over the lines and try to parse the data. The first two lines can be skipped because
+	// they should contain the identifier (see Identifier()) and a new-line.
+	// The last line is skipped because it should only contain an exclamation mark.
+	for i, l := range lines[2 : len(lines)-1] {
+		idCodeEnd := strings.Index(l, "(")
+		if idCodeEnd == -1 {
+			return nil, errors.New("Expected '(', not found on line" + string(i))
+		}
+
+		idCode := l[:idCodeEnd]
+
+		// The rest of the line is a number of values in round brackets "()".
+		// Let's use a simple split on ")(" to get those.
+		parts := strings.Split(l[idCodeEnd+1:len(l)-1], ")(")
+		result[idCode] = parts
+	}
+
+	return result, nil
+}
